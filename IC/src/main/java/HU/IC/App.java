74a6f237012d0de9b45fa6db28c5e4961f7c615a
sameer-khalili <sameer.khalili@student.hu.nl>
2019-02-22 12:20:44 +0100

    dds

--- IC/src/main/java/HU/IC/App.java
+++ IC/src/main/java/HU/IC/App.java
@@ -1,13 +1,13 @@
 package HU.IC;
 
 /**
- * Hello world!
+ * Test message 
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "HOi World!" );
     }
 }
